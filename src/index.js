0e167dc7223552f8b8d1cdc7388a19b6cbd242f3
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-28 02:13:12 +0200

    new

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './sass/index.scss';
+import { colorTheme } from './js/color_theme.js';
+
+const toggle = document.querySelector('.toggle');   
+
+
+toggle.addEventListener('click', colorTheme);
+
+// function checkColor(){
+//     if()
+// }
---
+++ src/js/color_theme.js
@@ -0,0 +1,42 @@
+export { colorTheme };
+
+const body = document.querySelector('body');
+const svgLight = document.querySelector('.svg__light');
+const svgDark = document.querySelector('.svg__dark');
+const inputT = document.querySelector('[input="search-input"]');
+const svgLupa = document.querySelector('.svg-lupa');
+const svgMenu = document.querySelector('.svg__menu');
+const svgX = document.querySelector('.svg__x');
+const svgSun = document.querySelector('.svg__sun');
+const svgMoon = document.querySelector('.svg__moon');
+
+//console.log(svgLupa);
+function colorTheme(){
+    body.classList.toggle('light__theme');
+    body.classList.toggle('dark__theme');//body
+
+    svgLight.classList.toggle('svg__light__theme');
+    svgLight.classList.toggle('svg__dark__theme');//light
+
+    svgDark.classList.toggle('svg__light__theme');
+    svgDark.classList.toggle('svg__dark__theme');//dark
+
+    inputT.classList.toggle('input__light__theme');
+    inputT.classList.toggle('input__dark__theme');//input
+
+    svgLupa.classList.toggle('lupa__light__theme');
+    svgLupa.classList.toggle('lupa__dark__theme');//лупа
+    
+    svgMenu.classList.toggle('lupa__light__theme');
+    svgMenu.classList.toggle('lupa__dark__theme');//menu burger
+
+    svgX.classList.toggle('lupa__light__theme');
+    svgX.classList.toggle('lupa__dark__theme');// X esc
+
+    // svgSun.classList.toggle('svg__sun__light__theme');
+    // svgSun.classList.toggle('svg__sun__dark__theme');
+
+    // svgMoon.classList.toggle('svg__moon__light__theme');
+    // svgMoon.classList.toggle('svg__moon__dark__theme');
+
+}
